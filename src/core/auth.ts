0ce1cc237acfcd52c0b89d4229869a17ec3e6f77
Yashodhan Singh <yashodhan.singh@unoapp.io>
2020-05-22 16:37:26 +0530

    add storage support in auth module

--- src/core/auth.ts
+++ src/core/auth.ts
@@ -11,16 +11,15 @@ import { ConfigError } from './exceptions/ConfigError'
 export class Auth {
   private _scheme: GenericObject
   private _strategy: AuthStrategy
+  private _storage: any
 
   /**
    * @constructor
    * @since 0.1.0
-   * @param {AuthStrategy} strategy The auth strategy
    * @param {GenericObject} scheme (optional) scheme followed by auth strategy
    */
-  constructor(strategy: AuthStrategy, scheme: GenericObject = {}) {
+  constructor(scheme: GenericObject) {
     this._scheme = scheme
-    this.setStrategy(strategy)
   }
 
   /**
@@ -44,6 +43,31 @@ export class Auth {
   }
 
   /**
+   * sets the storage of the instance.
+   *
+   * @public
+   * @since 0.1.0
+   * @param {any} storage the storage object for auth
+   * @returns instance
+   */
+  public setStorage(storage: any) {
+    this._storage = storage
+    return this
+  }
+
+  /**
+   * sets the auth strategy of the instance
+   *
+   * @since 0.1.0
+   * @param {AuthStrategy} strategy the auth strategy
+   * @returns instance
+   */
+  public setStrategy(strategy: AuthStrategy) {
+    this._setStrategy(strategy)
+    return this
+  }
+
+  /**
    * This method set auth strategy after making sure that the
    * strategy argument passes a valid strategy object
    *
@@ -54,7 +78,7 @@ export class Auth {
    * @throws {ConfigError} if a required property of strategy is not defined
    * @throws {TypeError} if the type of a property in strategy does not match the expected type
    */
-  private setStrategy(strategy: AuthStrategy) {
+  private _setStrategy(strategy: AuthStrategy) {
     this.validateStrategy(strategy)
     this._strategy = strategy
   }
